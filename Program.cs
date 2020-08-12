41ff23279d9a7745320387db7d5584706212eeab
Kyle Dick <kidick@mtu.edu>
2020-08-12 18:53:34 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace EAS_Decoder {
+	class Program {
+		static void Main(string[] args) {
+			//ProcessStartInfo startInfo = new ProcessStartInfo {
+			//	FileName = "sox/sox.exe",
+			//	Arguments = @"-V2 -V2 -t wav .\output.wav -t raw -esigned-integer -b16 -r 22050 .\output.raw remix 1",
+			//	WindowStyle = ProcessWindowStyle.Hidden,
+			//	UseShellExecute = false,
+			//	CreateNoWindow = false,
+			//	WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory
+			//};
+			//using (Process soxProcess = Process.Start(startInfo)) {
+			//	soxProcess.WaitForExit();
+			//}
+
+			string soxDirectory = null;
+
+			foreach (DriveInfo drive in DriveInfo.GetDrives()) {
+				string programFilesDirectory = $@"{drive.Name}Program Files (x86)";
+				if (Directory.Exists(programFilesDirectory)) {
+					string[] programFilesFolders = Directory.GetDirectories(programFilesDirectory);
+					foreach (string programFilesFolder in programFilesFolders) {
+						if (programFilesFolder.Contains("sox")) {
+							
+						}
+					}
+				}
+			}
+			Console.WriteLine("DONE");
+			Console.ReadKey();
+		}
+	}
+}
