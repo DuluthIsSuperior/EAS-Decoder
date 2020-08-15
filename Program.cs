b70c05cabc8aa71cd00fc4b2fd09a950edc311d9
Kyle Dick <kidick@mtu.edu>
2020-08-14 20:24:03 -0400

    Added arguments to allow users to specify an input file and its type; Currently only works with raw files

--- Program.cs
+++ Program.cs
@@ -4,23 +4,41 @@ using System.Runtime.CompilerServices;
 
 namespace EAS_Decoder {
 	class Program {
+		static void DisplayHelp() {
+			Console.Write("EASDecoder [args]\n\n" +
+						"Arguemnts:\n" +
+						"    -s [DIRECTORY]: Directory to sox.exe (not required if sox is installed in its default location on any drive)\n" +
+						"    -i, --input, or --input-file [FILEPATH]: Input file to analyze\n" +
+						"    -t, or --type [TYPE]: The type of the input file (assumed to be .raw if not specified)\n" +
+						"    -h or --help: Displays this help page\n");
+		}
 		static void Main(string[] args) {
 			string soxDirectory = null;
+			string inputFileDirectory = null;
+			string inputFileType = "raw";
 
 			for (int i = 0; i < args.Length; i++) {
 				if (args[i] == "-s") {
-					string possibleSoxDirectory = args[i + 1];
-					if (File.Exists(possibleSoxDirectory)) {
-						soxDirectory = args[i + 1];
-						i++;
+					i++;
+					if (File.Exists(args[i])) {
+						soxDirectory = args[i];
 					} else {
 						Console.WriteLine("Could not find sox.exe at the given directory");
 					}
+				} else if (args[i] == "-i" || args[i] == "--input-file" || args[i] == "--input") {
+					i++;
+					if (File.Exists(args[i])) {
+						inputFileDirectory = args[i];
+					} else {
+						Console.WriteLine($"Could not open the file at the given directory: {args[i]}");
+						DisplayHelp();
+						Environment.Exit(4);
+					}
+				} else if (args[i] == "-t" || args[i] == "--type") {
+					i++;
+					inputFileType = args[i];
 				} else if (args[i] == "-h" || args[i] == "--help") {
-					Console.Write("EASDecoder [args]\n\n" +
-						"Arguemnts:\n" +
-						"-s [DIRECTORY]: Directory to sox.exe (not required if sox is installed in its default location on any drive)\n" +
-						"-h or --help: Displays this help page\n");
+					DisplayHelp();
 					Environment.Exit(0);
 				} else {
 					Console.WriteLine($"Invalid argument: {args[i]}");
@@ -28,15 +46,19 @@ namespace EAS_Decoder {
 				}
 			}
 
+			if (inputFileDirectory == null) {
+				Console.WriteLine("An input file path is required");
+				DisplayHelp();
+				Environment.Exit(5);
+			}
 			soxDirectory = Sox.GetSoxProcess(soxDirectory);
 
-			Decode.DecodeEASTones("output.raw");
-			Decode.DecodeEASTones("output2.raw");
-			Decode.DecodeEASTones("anSVR.raw");
-			Decode.DecodeEASTones("aTOR.raw");
+			if (inputFileType != "raw") {
+				Console.WriteLine("raw files can only be used until integration with sox is complete");
+				Environment.Exit(6);
+			}
 
-			Console.WriteLine("DONE");
-			Console.ReadKey();
+			Decode.DecodeEASTones(inputFileDirectory);
 		}
 	}
 }
