dc2ed629297d256ed21acce16be6dc10505dfb1f
Pyrodron <25357202+Pyrodron@users.noreply.github.com>
2020-10-25 21:14:16 -0400

    Simplified code for arguments and help messages

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ namespace EAS_Decoder {
 						"Arguments:\n" +
 						" *  -s or --sox [FILEPATH]: Directory to sox.exe\n" +
 						" *  -f or --ffmpeg [FILEPATH]: Directory to ffmpeg.exe\n" +
-						"    -i or --input [FILEPATH]: Input file to analyze\n" +
+						" *  -i or --input [FILEPATH]: Input file to analyze\n" +
 						"    -t or --type [TYPE]: The type of the input file (assumed to be .raw if not specified)\n" +
 						"    -o or --output [FILEPATH]: Output file to convert input file to raw using sox\n" +
 						"    -r or --record: Saves recordings of EAS alerts that this program reads in using parameters from the input file\n" +
@@ -120,51 +120,81 @@ namespace EAS_Decoder {
 			Console.WriteLine("Done");
 		}
 
-		static void Main(string[] args) {
-			string inputFileDirectory = null;
-			string inputFileType = "raw";
-			string outputFileDirectory = null;
-			bool recordOnEAS = false;
-			uint duration = 5;
-
-			for (int i = 0; i < args.Length; i++) {
-				if (args[i] == "-s" || args[i] == "--sox") {
-					i++;
-					if (File.Exists(args[i])) {
-						ProcessManager.SoxDirectory = args[i];
-					} else if (args[i] == "-h" || args[i] == "--help") {
-						Console.WriteLine("\nUsage:\n    EASDecoder [-s or --sox] [DIRECTORY]\n\n" +
+		static void GetHelpForArgument(char arg) {
+			Console.Write("\nUsage:\n    EASDecoder ");
+			switch (arg) {
+				case 's':
+					Console.WriteLine("[-s or --sox] [DIRECTORY]\n\n" +
 							"The program sox is required to convert the incoming audio file into raw data for this program to decode EAS tones.\n" +
 							"If sox is not added to Path in the Environment Variables, then this flag with a valid path to sox.exe is required.\n\n" +
 							"DIRECTORY: Directory to sox.exe");
-						Environment.Exit(0);
-					} else {
-						Console.WriteLine("error -s: Could not find: {args[i]}\n");
-						Environment.Exit(13);
-					}
-				} else if (args[i] == "-f" || args[i] == "--ffmpeg") {
-					i++;
-					if (File.Exists(args[i])) {
-						ProcessManager.FfmpegDirectory = args[i];
-					} else if (args[i] == "-h" || args[i] == "--help") {
-						Console.WriteLine("\nUsage:\n    EASDecoder [-f or --ffmpeg] [DIRECTORY]\n\n" +
+					break;
+				case 'f':
+					Console.WriteLine("[-f or --ffmpeg] [DIRECTORY]\n\n" +
 							"The program ffmpeg is required to dump the incoming audio file's data for this program to decode EAS tones.\n" +
 							"It also allows streaming audio to be used and monitored in real time.\n" +
 							"If ffmpeg isn't added to Path in the Environment Variables, then this flag with a valid path to ffmpeg.exe is required.\n\n" +
 							"DIRECTORY: Directory to ffmpeg.exe");
-						Environment.Exit(0);
-					} else {
-						Console.WriteLine($"error -f: Could not find: {args[i]}\n");
-						Environment.Exit(14);
-					}
+					break;
+				case 'i':
+					Console.WriteLine("[-i or --input] [FILEPATH]\n\n" +
+							"Allows a local file or streaming URL to be read by this program.\n" +
+							"If your local file path contains spaces, please surround the file path in double quotation marks.\n\n" +
+							"FILEPATH: Full path leading to the file or URL of the streaming file");
+					break;
+			}
+			Environment.Exit(0);
+		}
+
+		static string GetSubArgument(string[] args, int index) {
+			if (index >= args.Length) {
+				return null;
+			}
+			return args[index];
+		}
+
+		static void NullExistsHelpElseDoAction(string flag, Action onFileFound, string subArgument, Action elseAction) {
+			if (subArgument == null) {
+				Console.WriteLine($"error {flag}: No other argument was found");
+				Environment.Exit(14);
+			} else if (File.Exists(subArgument)) {
+				onFileFound.Invoke();
+			} else if (subArgument == "-h" || subArgument == "-help") {
+				GetHelpForArgument(flag[1]);
+			} else {
+				elseAction.Invoke();
+			}
+		}
+
+		static void NullExistsHelpElseExit(string flag, Action onFileFound, string subArgument) {
+			NullExistsHelpElseDoAction(flag, onFileFound, subArgument, () => {
+				Console.WriteLine($"error {flag}: Could not find: {subArgument}\n");
+				Environment.Exit(15);
+			});
+		}
+
+		static string inputFileDirectory;
+		static string inputFileType = "raw";
+		static string outputFileDirectory = null;
+		static bool recordOnEAS = false;
+		static uint duration = 5;
+		static void ProcessArguments(string[] args) {
+			// TODO: Catch exception if user forgets to input secondary argument
+			for (int i = 0; i < args.Length; i++) {
+				string subArgument = GetSubArgument(args, i + 1);
+				if (args[i] == "-s" || args[i] == "--sox") {
+					i++;    // increment i at subargument
+					NullExistsHelpElseExit(args[i - 1], () => { ProcessManager.SoxDirectory = subArgument; }, subArgument);
+				} else if (args[i] == "-f" || args[i] == "--ffmpeg") {
+					i++;
+					NullExistsHelpElseExit(args[i - 1], () => { ProcessManager.FfmpegDirectory = subArgument; }, subArgument);
 				} else if (args[i] == "-i" || args[i] == "--input-file" || args[i] == "--input") {
 					i++;
-					if (File.Exists(args[i])) {
-						inputFileDirectory = args[i];
-					} else {
-						if (!ValidURL(args[i])) {
-							Console.WriteLine($"Could not open file or URL: {args[i]}");
+					NullExistsHelpElseDoAction(args[i - 1], () => { inputFileDirectory = subArgument; }, subArgument, () => {
+						if (!ValidURL(subArgument)) {
+							Console.WriteLine($"Could not open file or URL: {subArgument}");
 							Console.WriteLine("If your file path contains spaces, please put it in quotation marks");
+							Console.WriteLine("If your attempting to stream from a URL, try addding http:// or https:// to the beginning");
 							Environment.Exit(4);
 						}
 						Livestream = true;
@@ -172,12 +202,16 @@ namespace EAS_Decoder {
 						if (!SuppressInfo) {
 							Console.WriteLine($"info: Successfully pinged {inputFileDirectory}");
 						}
-					}
+					});
 				} else if (args[i] == "-u" || args[i] == "--update") {
 					GetSAMECodesFromInternet();
 				} else if (args[i] == "-t" || args[i] == "--type") {
 					i++;
-					inputFileType = args[i];
+					if (subArgument == null) {
+						Console.WriteLine("error -t: No other argument was found");
+						Environment.Exit(13);
+					}
+					inputFileType = subArgument;
 					if (inputFileType == "aac") {
 						Console.WriteLine("attn: ffmpeg refuses to output to aac; treating as mp3");
 						inputFileType = "mp3";
@@ -190,14 +224,22 @@ namespace EAS_Decoder {
 					}
 				} else if (args[i] == "-d" || args[i] == "--duration") {
 					i++;
-					if (!uint.TryParse(args[i], out duration)) {
-						Console.WriteLine("err: Could not understand input for duration. Please make sure a positive integer/whole number was typed in.");
+					if (subArgument == null) {
+						Console.WriteLine("error -d: No other argument was found");
+						Environment.Exit(13);
+					}
+					if (!uint.TryParse(subArgument, out duration)) {
+						Console.WriteLine("error -d: Could not understand input for duration. Please make sure a positive integer/whole number was typed in.");
 					}
 				} else if (args[i] == "-o" || args[i] == "--output" || args[i] == "--output-file") {
 					i++;
-					if (File.Exists(args[i])) {
+					if (subArgument == null) {
+						Console.WriteLine("error -o: No other argument was found");
+						Environment.Exit(13);
+					}
+					if (File.Exists(subArgument)) {
 						while (true) {
-							Console.Write($"warn: File already exists at {args[i]}. File will be overwritten! Continue (Y/N)? ");
+							Console.Write($"warn: File already exists at {subArgument}. File will be overwritten! Continue (Y/N)? ");
 							string response = Console.ReadKey().KeyChar.ToString().ToUpper();
 							if (response == "N") {
 								Console.WriteLine();
@@ -218,21 +260,28 @@ namespace EAS_Decoder {
 						Console.WriteLine("Please specify the input file's type before using the output flag");
 						Environment.Exit(0);
 					}
-					string outputPath = $"{args[i]}.{inputFileType}.raw";
+					string outputPath = $"{subArgument}.{inputFileType}.raw";
 					Console.WriteLine($"Output file will be written to {outputPath}");
 					outputFileDirectory = outputPath;
-				} else if (args[i] == "-u" || args[i] == "--update") {
-					GetSAMECodesFromInternet();
 				} else if (args[i] == "--suppress") {
 					SuppressInfo = true;
 				} else if (args[i] == "-c" || args[i] == "--convert") {
 					i++;
-					if (File.Exists(args[i])) {
-						ProcessManager.ConvertRAWToMP3(args[i], $"{args[i]}.mp3");
+					if (subArgument == null) {
+						Console.WriteLine("error -c: No other argument was found");
+						Environment.Exit(13);
+					}
+					if (File.Exists(subArgument)) {
+						ProcessManager.ConvertRAWToMP3(args[i], $"{subArgument}.mp3");
 					}
 					return;
 				} else if (args[i] == "-m" || args[i] == "--message") {
-					Console.WriteLine(args[++i]);	// shell will treat the string surrounded by " as one whole string
+					i++;
+					if (subArgument == null) {
+						Console.WriteLine("error -m: No other argument was found");
+						Environment.Exit(13);
+					}
+					Console.WriteLine(subArgument);   // shell will treat the string surrounded by " as one whole string
 				} else if (args[i] == "-h" || args[i] == "--help") {
 					DisplayHelp();
 					Environment.Exit(0);
@@ -241,6 +290,11 @@ namespace EAS_Decoder {
 					Environment.Exit(2);
 				}
 			}
+		}
+
+		static void Main(string[] args) {
+
+			ProcessArguments(args);
 
 			try {
 				string SAMEJson = File.ReadAllText("SAMECodes.json");
